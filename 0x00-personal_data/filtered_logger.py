3c44dc44c5e65f24010acc2b10dd1bced341edc6
stewie-gil <gilliganstewie7@gmail.com>
2023-08-31 06:26:01 -0700

    type annotate

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,9 +3,11 @@
 
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """matches and replaces fields with redaction"""
     pattern = r'({})=([^;]+)'.format('|'.join(fields))
     replacement = r'\1={}'.format(redaction)
