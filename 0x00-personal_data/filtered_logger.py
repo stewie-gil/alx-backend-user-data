75ca4e612c5c4c5756a1a3b51ee9bdb7ef0a3a28
stewie-gil <gilliganstewie7@gmail.com>
2023-08-31 06:32:08 -0700

    fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,6 +9,6 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """matches and replaces fields with redaction"""
-    pattern = r'({})=([^;]+)'.format('|'.join(fields))
-    replacement = r'\1={}'.format(redaction)
-    return re.sub(pattern, replacement, message)
+    pattern = "|".join(map(re.escape, fields))
+    return re.sub(f'({pattern})=[^\\{separator}]*',
+                  f'\\1={redaction}', message)
