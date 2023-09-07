4697cd2ee7b8f96488d3c54fa173d962d6452570
stewie-gil <gilliganstewie7@gmail.com>
2023-09-07 16:25:35 -0700

    base64 credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,26 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ inherits from auth"""
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Extract and return the Base64 part of the
+        Authorization header for Basic Authentication.
+
+        Args:
+            authorization_header (str): The Authorization header string.
+
+        Returns:
+            str: The Base64 part of the
+        Authorization header, or None if not valid.
+        """
+        if authorization_header is None:
+            return None
+        elif not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        base64_credentials = authorization_header[6:]
+        return base64_credentials
