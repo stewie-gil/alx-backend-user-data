06701cec01cfe68c179c8948633749d83eb4c609
stewie-gil <gilliganstewie7@gmail.com>
2023-09-06 17:47:45 -0700

    adding auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+""" the Authentication module"""
+
+
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """Authentication class for handling user authentication."""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+         Check if authentication is required for a given path.
+
+        Args:
+            path (str): The requested path.
+            excluded_paths (List[str]): List of paths excluded from auth.
+
+        Returns:
+            bool: True if authentication is required, False otherwise.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Retrieve the authorization header from the Flask request object.
+
+        Args:
+            request: The Flask request object.
+
+        Returns:
+            str: The authorization header or None if not present.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieve info about the current user based on the Flask request object.
+
+        Args:
+            request: The Flask request object.
+
+        Returns:
+            TypeVar('User'): Info about current user or None if not available.
+        """
+        return None
