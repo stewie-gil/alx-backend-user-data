14f8f7c88ae2dcbcd9fbe79dd03b64cff0db0935
stewie-gil <gilliganstewie7@gmail.com>
2023-09-09 14:10:01 -0700

    added session auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" the SessionAuth module"""
+
+from auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Session-based authentication mechanism.
+    """
+    pass
