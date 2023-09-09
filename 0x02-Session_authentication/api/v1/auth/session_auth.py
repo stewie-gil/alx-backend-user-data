949e01c7466781d00178da37122129f2e8532f64
stewie-gil <gilliganstewie7@gmail.com>
2023-09-09 15:25:22 -0700

    adding user_id_by_session attribute

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,24 @@
 
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     Session-based authentication mechanism.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates a Session ID for a user_id
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+
+        id = uuid.uuid4()
+        self.user_id_by_session_id[id] = user_id
+        return id
