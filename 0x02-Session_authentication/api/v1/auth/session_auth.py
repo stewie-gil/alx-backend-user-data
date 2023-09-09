25fc3b59fc218a8e9d1d5c149a889a0cbb971884
stewie-gil <gilliganstewie7@gmail.com>
2023-09-09 15:44:36 -0700

    retrieve sessio

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -24,3 +24,11 @@ class SessionAuth(Auth):
         id = uuid.uuid4()
         self.user_id_by_session_id[id] = user_id
         return id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """returns a User ID based on a Session ID"""
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
