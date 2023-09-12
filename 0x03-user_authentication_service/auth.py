0ce1da56b08ffc9876441181fdf96a2b2d88698a
stewie-gil <gilliganstewie7@gmail.com>
2023-09-12 15:16:19 -0700

    register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
+
 def _hash_password(password: str) -> bytes:
     """ Generates a slat and hash the password"""
     salt = bcrypt.gensalt()
@@ -19,31 +20,16 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    
-#    def register_user(self, email: str, password: str) -> User:
-#        """ Takes mandatory email and password string arguments and returns
-#        a User object.
-#        """
-#        result = self._db.find_user_by(email=email)  
-#        try:
-#            self._db.find_user_by(email=email)
-#            raise ValueError(f"User {email} already exists")
-#        except NoResultFound:
-#            return self._db.add_user(email, _hash_password(password))
-
-
     def register_user(self, email: str, password: str) -> User:
+        """checks if user is in the databases and adds them"""
         try:
             result = self._db.find_user_by(email=email)
             if result.email == email:
                 raise ValueError('User {} already exists'.format(email))
-            else:
-                hashed = _hash_password(password)
-                user = self._db.add_user(email, hashed)
-        except NoResultfound as e:
+        except NoResultFound:
+            hashed = _hash_password(password)
+            user = self._db.add_user(email, hashed)
             return user
 
-
-
     def valid_login(self, email: str, password: str) -> bool:
-        
+        pass
