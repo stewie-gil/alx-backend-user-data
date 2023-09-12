d471bbfb40f05909d05217a8562600148840da9e
stewie-gil <gilliganstewie7@gmail.com>
2023-09-12 10:32:27 -0700

    update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -38,7 +38,7 @@ class DB:
         self._session.commit()
         return userobj
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """ takes in arbitrary keyword arguments and
         returns the first row found in the users table"""
         try:
